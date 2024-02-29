f9851a47430e6698eb9f439a2ec40658b5b1c591
Nishant Mohan <nismohan@amd.com>
2024-02-29 10:55:56 +0000

    AMDGPU stub

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.h
@@ -66,6 +66,19 @@ public:
   getPredicatedAddrSpace(const Value *V) const override;
 
   unsigned getAddressSpaceForPseudoSourceKind(unsigned Kind) const override;
+
+  Error buildCodeGenPipeline(ModulePassManager &, raw_pwrite_stream &,
+                             raw_pwrite_stream *, CodeGenFileType,
+                             CGPassBuilderOption,
+                             PassInstrumentationCallbacks *) override;
+  
+
+  // bool parseLoopPass(StringRef, LoopPassManager &);
+
+  // std::pair<StringRef, bool> getPassNameFromLegacyName(StringRef) override;
+
+  // Error parseMIRPipeline(MachineFunctionPassManager &MFPM, 
+  //                                     StringRef PipelineText) override;
 };
 
 //===----------------------------------------------------------------------===//
